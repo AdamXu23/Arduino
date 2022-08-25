a48426cbb7c8e9ee113f7aa49b93048099798a54
AdamXu <xsz1993@live.com>
2022-08-25 13:40:29 +0800

    123

---
+++ PN532/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,10 @@
+# 1 "c:\\Users\\822\\Documents\\GitHub\\Arduino\\PN532\\NDEF_Reader.ino"
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
---
+++ PN532/build/sketch/NDEF_Reader.ino.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#line 1 "c:\\Users\\822\\Documents\\GitHub\\Arduino\\PN532\\NDEF_Reader.ino"
+#line 1 "c:\\Users\\822\\Documents\\GitHub\\Arduino\\PN532\\NDEF_Reader.ino"
+void setup();
+#line 6 "c:\\Users\\822\\Documents\\GitHub\\Arduino\\PN532\\NDEF_Reader.ino"
+void loop();
+#line 1 "c:\\Users\\822\\Documents\\GitHub\\Arduino\\PN532\\NDEF_Reader.ino"
+void setup()
+{
+	
+}
+
+void loop()
+{
+	
+}
+
